0b3d47303e1ca87b57731eb1a498b380b43fb3ec
Departamento Pessoal <departamentopessoal@otaviolage.com>
2026-05-22 16:03:33 -0300

    Update users.ts

--- src/mock/users.ts
+++ src/mock/users.ts
@@ -1,20 +1,20 @@
 export const localUsers = [
   {
-    id: 1,
+    id: '1',
     nome: 'Administrador',
     login: 'admin',
     senha: '123456',
     perfil: 'ADMIN'
   },
   {
-    id: 2,
+    id: '2',
     nome: 'Supervisor',
     login: 'supervisor',
     senha: '123456',
     perfil: 'SUPERVISOR'
   },
   {
-    id: 3,
+    id: '3',
     nome: 'Matheus',
     login: 'departamentopessoal@otaviolage.com',
     senha: '123456',
