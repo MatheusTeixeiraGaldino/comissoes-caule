195b43bdbda0947f48f6bfd439fe65b07218b0a5
Departamento Pessoal <departamentopessoal@otaviolage.com>
2026-05-22 15:35:26 -0300

    Create users.ts

---
+++ src/mock/users.ts
@@ -0,0 +1,16 @@
+export const localUsers = [
+  {
+    id: 1,
+    nome: 'Administrador',
+    login: 'admin',
+    senha: '123456',
+    perfil: 'ADMIN'
+  },
+  {
+    id: 2,
+    nome: 'Supervisor',
+    login: 'supervisor',
+    senha: '123456',
+    perfil: 'SUPERVISOR'
+  }
+]
